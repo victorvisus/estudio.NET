5c16f4453dd2bca57b824e1cab172210a00efc17
Victor Visús <victor.vxg@gmail.com>
2021-07-07 19:39:29 +0200

    Metodos

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Instanciar un objeto de la clase Program
+            Program objeto = new Program();
+
+            objeto.metodoPrivado();
+
+            objeto.metodoPublico();
+
+            Console.WriteLine(objeto.metodoReturn());
+
+            Console.WriteLine(objeto.metodoReturnParametros("Envio datos"));
+        }
+
+
+
+        // METODOS DE CLASE
+        private void metodoPrivado()
+        {
+            Console.WriteLine("Método Privado");
+        }
+
+        public void metodoPublico()
+        {
+            Console.WriteLine("Este es el metodo publico");
+        }
+
+        private String metodoReturn()
+        {
+            int numero = Convert.ToInt16("52");
+
+            String texto = "Metodo con return" + numero;
+            return texto;
+        }
+
+        private String metodoReturnParametros(String parametro)
+        {
+            String texto = parametro + " y los imprimo con más texto";
+
+            return texto;
+
+        }
+
+    }
+}
--- PROG02_Tarea/PROG02_Tarea/Program.cs
+++ PROG02_Tarea/PROG02_Tarea/Program.cs
@@ -13,13 +13,16 @@ namespace PROG02_Tarea
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             // Sexo: con dos valores posibles 'V' o 'M'  - char almacena un caracter, pesa 1bit.
+            char[] sexo = { 'V', 'M' };
 
             // Día de la semana 
+            String[] diasSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo" };
 
             // Si un empleado está casado o no. - boleano
             Boolean casado = false;
@@ -60,14 +63,19 @@ namespace PROG02_Tarea
 
 
             // Valor máximo no modificable: 999999. - constante: int
+            const int valorMax = 999999;
 
             // Día del año. - short
+            short diaAnio;
 
             // Milisegundos transcurridos desde el 01/01/1970 hasta nuestros días. - double
+            double tiempo;
 
             // Almacenar el total de una factura - float
+            float totalFra;
 
             // Población mundial del planeta tierra. - long
+            long personas;
 
 
 
