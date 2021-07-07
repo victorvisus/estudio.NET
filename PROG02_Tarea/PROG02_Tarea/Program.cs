4d82318b94e0358b81cbf35b423998271fd58c73
Victor Visús <victor.vxg@gmail.com>
2021-07-07 18:54:46 +0200

    Primer commit

---
+++ PROG02_Tarea/PROG02_Tarea/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+/**
+ * @author Victor
+ *
+ * EJERCIO 2 de la TAREA 2 Dado el siguiente programa, modifícalo para utilizar
+ * las variables que se indican. El tipo de dato elegido debe ser el de menos
+ * bits posibles que puedan representar el valor. Justifica tu elección.
+ * 
+ */
+
+namespace PROG02_Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Sexo: con dos valores posibles 'V' o 'M'  - char almacena un caracter, pesa 1bit.
+
+            // Día de la semana 
+
+            // Si un empleado está casado o no. - boleano
+            Boolean casado = false;
+
+            String respuesta;
+            Boolean flag = false;
+
+            do { 
+                Console.WriteLine("else empleado esta casado? (1 para Si; 2 para No)");
+                int resp = Convert.ToInt32(Console.ReadLine());
+                
+                switch (resp)
+                {
+                    case 1:
+                        casado = true;
+                        flag = true;
+                        break;
+                    case 2:
+                        casado = false;
+                        flag = true;
+                        break;
+                    default:
+                        Console.WriteLine("El valor introducido no es correcto");
+                        flag = false;
+                        break;
+                }
+            } while (flag == false) ;
+
+            if (casado == true)
+            {
+                respuesta = "Si";
+            } else
+            {
+                respuesta = "No";
+            }
+
+            Console.WriteLine("El empleado esta casado?\n {0}", respuesta);
+
+
+            // Valor máximo no modificable: 999999. - constante: int
+
+            // Día del año. - short
+
+            // Milisegundos transcurridos desde el 01/01/1970 hasta nuestros días. - double
+
+            // Almacenar el total de una factura - float
+
+            // Población mundial del planeta tierra. - long
+
+
+
+        }
+    }
+}
